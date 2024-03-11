00ce100362e23ce4747578d40dba9366de97a867
bhatiadheeraj <dheerajbhatia2010@gmail.com>
2024-03-11 21:06:11 +0000

    added import_dataset dl_dataset_query_item api

--- pybrainlife/api/datalad.py
+++ pybrainlife/api/datalad.py
@@ -1,13 +1,20 @@
 from typing import List, Dict, Any, Optional
 from datetime import datetime
 from dataclasses import dataclass
+
 from .utils import is_id, nested_dataclass, hydrate
 from .api import auth_header, services
 import requests
 import json
 
+def dl_dataset_fetch(id: str) -> Optional["DLDataset"]:
+    dataset = dl_datasets_query(id=id)
+    if len(dataset) == 0:
+        return None
+    return dataset[0]
 
-def dl_dataset_query(
+
+def dl_datasets_query(
     id: Optional[str] = None,
     path: Optional[str] = None,
     commit_id: Optional[str] = None,
@@ -24,7 +31,7 @@ def dl_dataset_query(
     task: Optional[str] = None,
     skip: int = 0,
     limit: int = 100,
-) -> List["DLItem"]:
+) -> List["DLDataset"]:
     query = {}
 
     if search:
@@ -89,6 +96,48 @@ def dl_dataset_query(
     return DLDataset.normalize(res.json())    
 
 
+def dl_import_dataset(
+    id: str,
+    project: str,
+    datatypes: List[str]
+):
+    url = services['warehouse'] + '/datalad/import/' + id
+
+    res = requests.post(url, json={"project": project, "datatypes": datatypes}, headers={**auth_header()})
+
+    if res.status_code != 200:
+        raise Exception(res.json()["message"])
+
+    return res.json()
+
+def dl_dataset_query_item(id: str) -> "DLItem":
+    url = services['warehouse'] + '/datalad/items/'
+    find = json.dumps({"dldataset": id})
+    select = 'dataset.meta.subject dataset.meta.session dataset.desc dataset.datatype dataset.datatype_tags dataset.tags',
+    res = requests.get(url, params={"find": find,
+                                    "select": select,
+                                    "limit": 1,
+                                    
+    }, headers={**auth_header()})
+
+# [
+#     {
+#         "_id": "6515f5bf257c5492c2828c56",
+#         "dataset": {
+#             "datatype": "594c0325fa1d2e5a1f0beda5",
+#             "datatype_tags": [],
+#             "meta": {
+#                 "subject": "M2001",
+#                 "session": "a1236"
+#             },
+#             "desc": "sub-M2001_ses-a1236_T2w.nii",
+#             "tags": []
+#         }
+
+    if res.status_code != 200:
+        raise Exception(res.json()["message"])
+    return DLItem.normalize(res.json())
+
 
 class DatasetDescription:
     Name: str
@@ -128,16 +177,15 @@ class Stats:
         stats.datatypes = data["datatypes"]
         return stats
 
+@hydrate(dl_dataset_fetch)
+@nested_dataclass
 class DLDataset:
     id: str
     path: str
     commit_id: str
     version: str
-    README: str
-    CHANGES: str
     dataset_description: DatasetDescription
     participants: List[Dict[str, Any]]
-    participants_info: Dict[str, Any]
     stats: Stats
     import_count: int
     create_date: datetime
@@ -147,34 +195,48 @@ class DLDataset:
     def normalize(data):
         if isinstance(data, list):
             return [DLDataset.normalize(d) for d in data]
-        print("Data received for normalization:", data)  # Add this print statement
-        dataset = DLDataset()
-        dataset.id = data["_id"]
-        dataset.path = data["path"]
-        dataset.commit_id = data["commit_id"]
-        dataset.version = data["__v"]
-        dataset.README = data["README"] if "README" in data else ""
-        dataset.CHANGES = data["CHANGES"] if "CHANGES" in data else ""
-        dataset.dataset_description = DatasetDescription.normalize(data["dataset_description"])
-        dataset.participants = data["participants"]
-        dataset.participants_info = data["participants_info"] if "participants_info" in data else {}
-        dataset.stats = Stats.normalize(data["stats"])
-        dataset.import_count = data["import_count"]
-        dataset.create_date = data["create_date"]
-        dataset.removed = data["removed"]
-        return dataset
+        data["id"] = data["_id"]
+        data["version"] = data["__v"]
+        data["dataset_description"] = DatasetDescription.normalize(data["dataset_description"])
+        data["stats"] = Stats.normalize(data["stats"])
+        data["create_date"] = data["create_date"]
+        return DLDataset(**data)
 
+@dataclass
+class DatasetMeta:
+    subject: str
+    session: str
+
+
+@dataclass
+class Dataset:
+    datatype: str
+    datatype_tags: List[str]
+    meta: DatasetMeta
+    desc: str
+    tags: List[str]
+
+
+@dataclass
 class DLItem:
-    dldataset: DLDataset
-    dataset: Dict[str, Any]
-    create_date: datetime
-    update_date: datetime
+    id: str
+    dataset: Dataset
 
     @staticmethod
-    def normalize(data: Dict[str, Any]) -> 'DLItem':
-        item = DLItem()
-        item.dldataset = DLDataset.normalize(data.get("dldataset", {}))
-        item.dataset = data["dataset"]
-        item.create_date = data["create_date"]
-        item.update_date = data["update_date"]
-        return item
+    def normalize(data: Dict[str, Any]) -> "DLItem":
+        if isinstance(data, list):
+            return [DLItem.normalize(d) for d in data]
+        
+        dataset = data['dataset']
+        meta = dataset['meta']
+        
+        return DLItem(
+            id=data['_id'],
+            dataset=Dataset(
+                datatype=dataset['datatype'],
+                datatype_tags=dataset.get('datatype_tags', []),
+                meta=DatasetMeta(subject=meta['subject'], session=meta['session']),
+                desc=dataset['desc'],
+                tags=dataset.get('tags', [])
+            )
+        )
